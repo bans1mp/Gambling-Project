1fca46dc5db02f112d053609a6cafeab2b866ef7
bans1mp <iamsarthakgandotra@gmail.com>
2022-01-14 13:58:53 +0530

    backend database update

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
+//Express setup
 const express = require('express')
 const app = express();
 const path = require('path');
-const mongoose = require('mongoose')
-const ejsMate = require('ejs-mate')
-const db = mongoose.connection
-const UserInfo = require('./models/userInfo')
-const methodOverride = require('method-override');
-mongoose.connect('mongodb://localhost:27017/gamblingApp', {});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//Mongoose Setup
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/gamblingApp', {});
+const db = mongoose.connection
 db.on("error", console.error.bind(console, "Connection error: "));
 db.once("open", () => {
     console.log("database connected")
 })
 
+//EJS Setup
+const ejsMate = require('ejs-mate')
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }));
+//Method Override Setup
+const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+//Model Require
+const UserInfo = require('./models/userInfo');
+const { userInfo } = require('os');
 
+//Requests
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -29,26 +37,51 @@ app.get('/roulette/start', (req, res) => {
     res.render('roulette/start')
 })
 
-app.get('/roulette/main', (req, res) => {
-    res.render('roulette/main');
-})
 
-app.post('/roulette/main', async (req, res) => {
+
+function printlol() {
+    console.log('lmao');
+}
+
+app.post('/roulette/start', async (req, res) => {
     const userInfo = new UserInfo({ name: req.body.name, highScore: 5000, game: 'Roulette' });
     await userInfo.save();
-
-
     res.render(`roulette/main`, { userInfo })
+})
+
+
+
+
+
+
+app.get('/50-50/start', (req, res) => {
+    res.render('50-50/start')
 })
 
 app.get('/50-50/main', (req, res) => {
     res.render('50-50/main')
 })
 
+app.post('/50-50/main', async (req, res) => {
+
+})
+
+
 app.get('/highscores', async (req, res) => {
     const allScores = await UserInfo.find();
     res.render('highscores', { allScores })
 })
+
+app.put('/highscores', async (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    const { id, score } = req.body
+    await UserInfo.findByIdAndUpdate(id, { highScore: score });
+    res.redirect('highscores')
+})
+
+app.post('/highscores',)
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 })
--- seeds/index.js
+++
@@ -1,1 +0,0 @@
-//probabilities are 7/15 7/15 and 1/15
